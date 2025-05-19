910de69c0364147b50a70d60322a7b973f1f0578
SANDRA DWI ASTUTI <151292658+sandradwiastuti@users.noreply.github.com>
2025-05-19 19:56:28 +0700

    Add files via upload

---
+++ form login/login.php
@@ -0,0 +1,17 @@
+
+<?php
+session_start();
+include 'koneksi/db.php';
+$username = $_POST['username'];
+$password = $_POST['password'];
+$query = "SELECT * FROM users WHERE username='$username'";
+$result = mysqli_query($conn, $query);
+$user = mysqli_fetch_assoc($result);
+if ($user && password_verify($password, $user['password'])) {
+    $_SESSION['user'] = $user['username'];
+    header("Location: dashboard.php");
+} else {
+    header("Location: index.php?error=1");
+    exit;
+}
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form method="post" action="tampil.php">
+		<label>Masukkan nama</label>
+		<input type="text" name="nama">
+
+		<label>Masukkan usia</label>
+		<input type="text" name="usia">
+
+		<label>Masukkan NPM</label>
+		<input type="text" name="npm">
+
+		<label>Masukkan alamat</label>
+		<input type="text" name="alamat">
+
+		<input type="submit">
+	</form>
+</body>
+</html>
